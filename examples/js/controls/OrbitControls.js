0741e75774f990026162772acae1e47de0b4ffde
guykastenbaum <guykastenbaum@users.noreply.github.com>
2015-01-20 23:24:44 +0100

    enable default right-click context menu when control is not enabled

--- examples/js/controls/OrbitControls.js
+++ examples/js/controls/OrbitControls.js
@@ -655,7 +655,7 @@ THREE.OrbitControls = function ( object, domElement ) {
 
 	}
 
-	this.domElement.addEventListener( 'contextmenu', function ( event ) { event.preventDefault(); }, false );
+	this.domElement.addEventListener( 'contextmenu', function ( event ) { if ( _this.enabled !== false ) event.preventDefault(); }, false );
 	this.domElement.addEventListener( 'mousedown', onMouseDown, false );
 	this.domElement.addEventListener( 'mousewheel', onMouseWheel, false );
 	this.domElement.addEventListener( 'DOMMouseScroll', onMouseWheel, false ); // firefox
